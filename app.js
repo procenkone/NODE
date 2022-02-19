2b50d67273dc8d4af0eda77960eda643011c195d
procenkone <procenkon@gmail.com>
2022-02-19 17:40:55 +0200

    hw_2

---
+++ app.js
@@ -0,0 +1,76 @@
+// 1. /login, поля які треба відрендерити в файлі hbs: firstName, lastName,
+// email(унікальне поле), password, age, city
+// просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися
+// в масив і редірект робити на сторінку з усіма юзерами /users і перевірка чи такий
+// імейл не існує, якщо існує то редірект на еррор пейдж
+// 2. /users просто сторінка з усіма юзерами, але можна по квері параметрам їх
+// фільтрувати по age і city
+// 3. /user/:id сторінка з інфою про одного юзера
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('express-handlebars');
+const {engine} = require('express-handlebars');
+const {json} = require("express");
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'));
+
+app.use(json());
+app.use(express.urlencoded({extended: true}));
+
+const users = [];
+
+app.get('/login', (req, res) => {//перехід на сторінку з формою
+    res.render('login');
+});
+
+app.post('/login', ({body}, res) => {//якщо відпрацював метод пост то перевірим чи є такий емейл і якщо є то переведем на сторінку помилки а якщо ні то запушим інфу в масив
+    const exist = users.some(user => user.email === body.email)//перевіряєм чи є в масиві такий емейл
+    if (exist) {
+        res.redirect('/error');//якщо є переводим на сторінку еррори
+        return;
+    }
+    users.push({...body, id: new Date().getSeconds()});//пушим данні в масив і добавляємо id
+    res.redirect('/users');//переводим на всіх юзерів
+});
+
+app.get('/users', ({query}, res) => {//передаєм весь масив на сторінку для відмальрвки
+    if (Object.keys(query).length) {
+        let user = [...users];
+        if (query.city) {
+            user = user.filter(user => user.city === query.city);
+        }
+        if (query.age) {
+            user = user.filter(user => user.age === query.age);
+        }
+        res.render('users', {users: user});
+        return
+    }
+    res.render('users', {users});
+});
+
+app.get('/users/:id', ({params}, res) => {//сторінка окремого юзера, дестректуризуєм з парамс айдішку
+    const user = users.find(user => user.id === +params.id);// перевіряєм чи в масиві є такий юзер і кладем в змінну(добавляєм "+" щоб перевести в цифру, бо дістаєм стрінгу )
+    if (!user) {//якщо юзера з такою айдішкою не має то:
+        res.redirect('/error')//переведи на ерору
+        return//закінчи подальше виконання коду
+    }
+    res.render('userInfo', {user});//передаєм на сторінку найденого юзера для відмальовки
+});
+
+app.get('/error', (req, res) => {//метод відмальовки сторінки з error
+    res.render('errorPage');
+});
+
+app.use((req, res) => {//сторінка з неіснуючим шляхом
+    res.render('notFound');
+});
+
+app.listen(3001, () => {//запуск сервера
+    console.log('server has started on port 3001');
+});
+
